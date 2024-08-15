e2b61f0d9573208978478b19a096c64e31ab27ee
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-08-15 10:37:59 +0300

    Add project files.

---
+++ PatikaWeek5Homework3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//string liste tanımlanıyor...
+List<string> coffees = new List<string>();
+
+Console.WriteLine("Lütfen 5 adet kahve ismi giriniz..:");
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"Kahve {i}..:");
+    coffees.Add(Console.ReadLine());//listeye kahve adı ekleniyor..
+}
+
+//kullanıcı tarafından girilmiş 5 kahve adı ekrana yazdırılıyor...
+Console.WriteLine("\nGirdiğiniz kahve isimleri:");
+foreach (string coffee in coffees)
+{
+    Console.WriteLine(coffee);
+}
